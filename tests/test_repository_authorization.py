ffa392155f5029c63aa88a3b40ed5e9e582eac88
Pabitra Dash <pabitra.dash@usu.edu>
2024-12-17 10:52:46 -0500

    Fix for zenodo token tests (#227)
    * using 401 status code in tests for missing or expired zenodo token
    
    * temporarily outputting test run outputs to console for debugging
    
    * check for invalid user cookie in test
    
    * check user cookie is valid json before using in request
    
    * Revert "temporarily outputting test run outputs to console for debugging"
    
    This reverts commit 7ffa618b059f3916879c804083a1bcd48b4290e4.

--- tests/test_repository_authorization.py
+++ tests/test_repository_authorization.py
@@ -1,3 +1,4 @@
+import json
 from unittest.mock import patch
 from urllib.parse import unquote
 
@@ -34,7 +35,7 @@ async def test_get_access_token_not_found(client_test, user_cookie):
     response = await client_test.get(
         prefix + "/access_token/zenodo?access_token=" + user_cookie, follow_redirects=False
     )
-    assert response.status_code == 404
+    assert response.status_code == 401
 
 
 async def test_get_access_token(client_test, user_cookie, authorize_response):
@@ -52,7 +53,12 @@ async def test_get_access_token_expired(client_test, user_cookie, authorize_resp
     with patch.object(StarletteRemoteApp, 'authorize_access_token', return_value=authorize_response_expired):
         response = await client_test.get(prefix + "/auth/zenodo?access_token=" + user_cookie, follow_redirects=False)
 
+    try:
+        json.loads(user_cookie)
+    except json.JSONDecodeError:
+        pytest.skip("User cookie is invalid")
+
     response = await client_test.get(
         prefix + "/access_token/zenodo?access_token=" + user_cookie, follow_redirects=False
     )
-    assert response.status_code == 404
+    assert response.status_code == 401
